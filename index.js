9af3cf9128c914825a05ce9f4a624ff15dead75a
Ib-bot-lab <155330651+Ib-bot-lab@users.noreply.github.com>
2024-12-30 13:07:19 +0100

    Updated function logic

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+
 export default async ({ req, res, log, error }) => {
   // Log request details for debugging
   log(req.bodyText); // Raw request body, contains request data
-  log(JSON.stringify(req.bodyJson)); // Object from parsed JSON request body, otherwise string
-  log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
-  log(req.scheme); // Value of the x-forwarded-proto header, usually http or https
-  log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
-  log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-  log(req.host); // Hostname from the host header, such as awesome.appwrite.io
-  log(req.port); // Port from the host header, for example 8000
-  log(req.path); // Path part of URL, for example /v1/hooks
-  log(req.queryString); // Raw query params string. For example "limit=12&offset=50"
-  log(JSON.stringify(req.query)); // Parsed query params. For example, req.query.limit
 
   try {
     // Parse the request payload
@@ -19,6 +16,18 @@ export default async ({ req, res, log, error }) => {
 
     // Extract numbers from the payload
     const { phoneNmber, otp } = payload;
+    let msgOption = {
+      from: TWILIO_PHONE_NUMBER,
+      to: phoneNmber,
+      body: `Hello Your code is  ${otp}`,
+    };
+
+    try {
+      const message = await client.messages.create(msgOption);
+      console.log("yeee", message);
+    } catch (error) {
+      console.log("errrrrr", error);
+    }
 
     log("jjjjjjjjjjjjjkkkk", phoneNmber, otp, process.env.APPWRITE_API_KEY);
 
